e8100eb88aa4229b2c62bb4674e3c1ac940b0f89
Jimmy <mill7382@umn.edu>
2019-05-15 22:43:37 -0500

    Added a counter to the function.

--- randlist.py
+++ randlist.py
@@ -1,6 +1,8 @@
 import random
 def randomlist():
     newlist = []
+    count = 0
     for i in range(5):
         newlist.append(random.randint(1,5))
-    return(newlist)
+        count +=1
+    return(newlist,count)
